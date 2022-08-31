862d088245e75f694ef1d934f47acbc15d65fae6
Jacana (roen-odoo) <robin.engels@skynet.be>
2022-08-31 15:09:54 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt install jq
+touch config.json
+sudo ln ./bin/* /usr/local/bin/ 2> /dev/null
+sudo ln ./python/* /usr/local/bin/ 2> /dev/null
+sudo ln ./config.json /usr/local/bin/ 2> /dev/null
